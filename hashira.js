9d34197163d48a6619af6cbf9c74a554679157d9
Mohd Abdul Nizam Qadri <138662521+binary-sorcerer@users.noreply.github.com>
2024-10-11 18:41:32 +0530

    Add files via upload

---
+++ hashira.js
@@ -0,0 +1,7 @@
+function showDetails(id) {
+    document.getElementById('details-' + id).style.opacity = '1';
+}
+
+function hideDetails(id) {
+    document.getElementById('details-' + id).style.opacity = '0';
+}
